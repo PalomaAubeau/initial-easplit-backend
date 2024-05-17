f3906b96d7e8e8d913e278065fd1d2fa285e43fa
Paloma <paloma.aubeau@gmail.com>
2024-05-17 20:29:02 +0200

    first commit

---
+++ models/users.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  password: String,
+  token: String,
+  phone: Number,
+  email: String,
+  balance: Number, //zéro par défaut au départ
+  events: [{ type: mongoose.Schema.Types.ObjectId, ref: "events" }],
+});
+
+const User = mongoose.model("users", userSchema);
+
+module.exports = User;
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var router = express.Router();
+
+require("../models/connection");
+//const User = require("../models/users");
+//const { checkBody } = require("../modules/checkBody");
+//const bcrypt = require("bcrypt");
+//const uid2 = require("uid2");
+//const uniqid = require("uniqid");
+//const cloudinary = require("cloudinary").v2;
+//const fs = require("fs");
+
+module.exports = router;
