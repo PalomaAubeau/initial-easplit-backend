037e35510adc40142c31ce629694646a4a2d3963
Yanis <yanisbenazz1@gmail.com>
2024-05-20 11:45:36 +0200

    route signup

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+
+require("../models/connection");
+const User = require("../models/users");
+const { checkBody } = require("../modules/checkBody");
+const uid2 = require("uid2");
+const bcrypt = require("bcrypt");
+
+router.post("/signup", (req, res) => {
+  if (!checkBody(req.body, ["password", "firstName", "email"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+});
