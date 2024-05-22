9c12636ac4b8d7cd5cadac7a3a1d0d26d5bcdb42
Marwane <70036240+Marwane77@users.noreply.github.com.>
2024-05-22 14:36:02 +0200

    bug routes transactions qui ne s'affichent pas chez user et event

--- routes/transactions.js
+++ routes/transactions.js
@@ -17,22 +17,26 @@ const handleResponse = (res, success, data) => {
   }
 };
 
-// Fonction pour gérer les transactions
+// Fonction pour gérer la sauvegarde des transactions afin d'éviter les répétitions dans la route
 const handleTransaction = (transaction, res, user = null, event = null) => {
   transaction.save().then((transaction) => {
+    // Vérification de l'existence de l'utilisateur
     if (user) {
-      // Vérifier si la transaction est déjà dans les transactions de l'utilisateur
+      // Vérification de l'existence de la transaction dans les transactions de l'utilisateur
       if (!user.transactions.includes(transaction._id)) {
+        // Ajout de la transaction aux transactions de l'utilisateur
         user.transactions.push(transaction._id);
       }
       user.save().then(() => {
+        // Vérification de l'existence de l'événement
         if (event) {
-          // Vérifier si la transaction est déjà dans les transactions de l'événement
+          // Vérification de l'existence de la transaction dans les transactions de l'événement
           if (!event.transactions.includes(transaction._id)) {
             event.transactions.push(transaction._id);
-            // Mettre à jour le totalSum de l'événement
+            // Mise à jour du totalSum de l'événement
             event.totalSum += transaction.amount;
           }
+          // Sauvegarde de l'événement
           event.save().then(() => {
             handleResponse(res, true, { transaction });
           });
@@ -41,12 +45,13 @@ const handleTransaction = (transaction, res, user = null, event = null) => {
         }
       });
     } else if (event) {
-      // Vérifier si la transaction est déjà dans les transactions de l'événement
+      // Vérification de l'existence de la transaction dans les transactions de l'événement
       if (!event.transactions.includes(transaction._id)) {
         event.transactions.push(transaction._id);
-        // Mettre à jour le totalSum de l'événement
+        // Mise à jour du totalSum de l'événement
         event.totalSum += transaction.amount;
       }
+      // Sauvegarde de l'événement
       event.save().then(() => {
         handleResponse(res, true, { transaction });
       });
@@ -58,75 +63,81 @@ const handleTransaction = (transaction, res, user = null, event = null) => {
 
 // Route pour créer une transaction
 router.post("/create", (req, res) => {
-  // Vérifier si les champs sont remplis
+  // Vérification si les champs sont remplis
   if (!checkBody(req.body, ["date", "type", "emitter"])) {
     return handleResponse(res, false, { error: "Champs manquants ou vides" });
   }
-  // Vérifier si le type de transaction est valide
+  // Vérification si le type de transaction est valide
   if (
-    req.body.type !== "refound" &&
+    req.body.type !== "refound" && 
+    // Vérification si le montant est un nombre et s'il a plus de 2 décimales après la virgule
     (req.body.amount.toString().split(".")[1] || "").length > 2
   ) {
+    // Envoi d'une erreur si le montant a plus de 2 décimales
     return handleResponse(res, false, {
       error: "Le solde ne peut pas avoir plus de 2 décimales",
     });
   }
-  // Créer une nouvelle transaction
+  // Création d'une nouvelle transaction
   const transaction = new Transaction(req.body);
-  // Vérifier le type de transaction
+  // Vérification du type de transaction
   switch (req.body.type) {
     case "reload":
-      // Vérifier si l'utilisateur existe
+      // Vérification si l'utilisateur existe
       User.findById(req.body.emitter).then((user) => {
+        // Envoi d'une erreur si l'utilisateur n'existe pas
         if (!user) {
           return handleResponse(res, false, {
             error: "Utilisateur non trouvé",
           });
         }
-        // Ajouter le montant au solde de l'utilisateur
+        // Ajout du montant au solde de l'utilisateur
         user.balance += Number(req.body.amount);
-        // Vérifier si le solde est négatif
+        // Vérification si le solde est négatif
         if (user.balance < 0) {
+        // Envoi d'une erreur si le solde est négatif
           return handleResponse(res, false, {
             error: "Opération impossible : solde négatif",
           });
         }
-        // Sauvegarder l'utilisateur
+        // Sauvegarde de la transaction et l'utilisateur
         user.save().then(() => handleTransaction(transaction, res, user, user));
       });
       break;
 
     case "payment":
-      // Vérifier si l'utilisateur existe
+      // Vérification si l'utilisateur existe
       User.findById(req.body.emitter).then((user) => {
         if (!user) {
           return handleResponse(res, false, {
             error: "Utilisateur non trouvé",
           });
         }
-        // Vérifier si l'utilisateur a assez de solde
+        // Vérification si le montant est supérieur au solde de l'utilisateur
         if (user.balance < Number(req.body.amount)) {
           return handleResponse(res, false, { error: "Solde insuffisant" });
         }
-        // Déduire le montant du solde de l'utilisateur
+        // Déduction du montant sur le solde de l'utilisateur
         user.balance -= Number(req.body.amount);
-        // Vérifier si le solde est négatif
+        // Vérification si le solde est négatif
         if (user.balance < 0) {
           return handleResponse(res, false, {
             error: "Opération impossible : solde négatif",
           });
         }
-        // Ajouter la transaction aux transactions de l'utilisateur
+        // Ajout de la transaction aux transactions de l'utilisateur
         user.transactions.push(transaction);
-        // Sauvegarder l'utilisateur
+        // Sauvegarde de la transaction et l'utilisateur
         user.save().then(() => {
-          // Vérifier si l'événement existe
+          // Vérification si le destinataire existe
           Event.findById(req.body.recipient).then((event) => {
             if (!event) {
+              // Envoi d'une erreur si le destinataire n'existe pas
               return handleResponse(res, false, {
                 error: "Événement non trouvé",
               });
             }
+            // Ajout du montant au solde de l'événement
             handleTransaction(transaction, res, user, event);
           });
         });
@@ -134,11 +145,14 @@ router.post("/create", (req, res) => {
       break;
 
     case "refound":
-      // Vérifier si l'événement existe
+      // Vérifcaton si l'événement existe
       Event.findById(req.body.emitter).then((event) => {
+        // Envoi d'une erreur si l'événement n'existe pas
         if (!event) {
+          // Envoi d'une erreur si l'événement n'existe pas
           return handleResponse(res, false, { error: "Événement non trouvé" });
         }
+        // Vérification si l'événement a des invités et donc des parts
         if (event.shareAmount === 0) {
           return handleResponse(res, false, {
             error: "Opération impossible : aucun invité à rembourser",
@@ -149,45 +163,50 @@ router.post("/create", (req, res) => {
         let promises = event.guests.map((guest) => {
           // Vérifier si l'utilisateur est un invité
           return User.findById(guest.userId).then((user) => {
-            // Ajouter le montant au solde de l'utilisateur
+            // Vérifier si l'utilisateur existe
             if (user) {
+              // Ajouter le montant à l'utilisateur
               user.balance += perShareAmount * guest.share;
               return user.save();
             }
           });
         });
+        // Nous attendons que toutes les promesses soient résolues
         Promise.all(promises).then(() => {
-          // Réinitialiser le solde de l'événement
+          // Réinitialisation du totalSum de l'événement
           event.totalSum = 0;
-          // Vérifier si le solde de l'événement est un nombre
+          // Envoi d'une erreur si le totalSum n'est pas un nombre
           if (isNaN(event.totalSum)) {
             return handleResponse(res, false, { error: "Opération invalide" });
           }
+          // Sauvegarde de l'événement
           event.save().then(() => handleTransaction(transaction, res, event));
         });
       });
       break;
 
     case "expense":
-      // Vérifier si l'événement existe
+      // Vérification si l'événement existe
       Event.findById(req.body.emitter).then((event) => {
+        // Envoi d'une erreur si l'événement n'existe pas
         if (!event) {
           return handleResponse(res, false, { error: "Événement non trouvé" });
         }
-        // Vérifier si l'événement a assez de totalSum
+        // Vérification si le montant est supérieur au totalSum de l'événement
         if (event.totalSum < Number(req.body.amount)) {
           return handleResponse(res, false, {
             error: "Fonds de l'événement insuffisants",
           });
         }
-        // Déduire le montant du totalSum de l'événement
+        // Déduction du montant du totalSum de l'événement
         event.totalSum -= Number(req.body.amount);
-        // Sauvegarder l'événement
+        // Sauvegarde de l'événement
         event.save().then(() => handleTransaction(transaction, res, event));
       });
       break;
 
     default:
+      // Envoi d'une erreur si le type de transaction est invalide
       return handleResponse(res, false, {
         error: "Type de transaction invalide",
       });
