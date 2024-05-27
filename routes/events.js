b23d612733506e1e918a864f9f754958eb2e0c21
Marwane <70036240+Marwane77@users.noreply.github.com.>
2024-05-27 14:22:08 +0200

    commit before pull

--- routes/events.js
+++ routes/events.js
@@ -157,7 +157,7 @@ router.get("/user-events/:token", (req, res) => {
 router.get("/event/:id", (req, res) => {
   Event.findById(req.params.id)
     .populate("organizer", ["firstName", "email"])
-    .populate("guests", ["firstName", "email", "share", "hasPaid"]) //Récupération des champs qui nous intéresse dans l'object
+    .populate("guests.userId", ["firstName", "email", "share", "hasPaid"]) //Récupération des champs qui nous intéresse dans l'object
     .populate("transactions")
     .then((event) => {
       if (!event) {
